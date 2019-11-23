d86860e9f065958aa7d7e85814c7b80439c9aade
Semyon Vervejn <semen_vervein@mail.ru>
2019-11-24 00:27:04 +0700

    Add new boss and new system create bosses and levels! Modificated more things!

--- Content/Constants.cs
+++ Content/Constants.cs
@@ -12,11 +12,14 @@ namespace ComicDefender
     {
         public Boss Boss1;
         Animation AnimationBoss1;
+        public Boss Boss2;
 
         public Constants()
         {
             AnimationBoss1 = new Animation(Program.content.GetBoss1(), 0, 0, 338, 338, 3, 0.02f);
             Boss1 = new Boss(Program.content.GetBoss1(), AnimationBoss1, 0, 0, 0, 0.3f, 0, 10000, 0, "Boss1");
+            Boss2 = new Boss(Program.content.GetBoss2(), 0, 0, 0, 0.3f, 0, 10000, 0, "Boss2");
+
         }
     }
 }
